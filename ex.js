f4b2af169fb088d26377e38c8e223bc90180b1ca
Nlucky320 <luckyes144@gmail.com>
2024-10-06 12:57:46 +0600

    fifth commit

--- ex.js
+++ ex.js
@@ -271,10 +271,18 @@
 //   console.log("The number is zero.");
 // }
 
-const start = 10;
-const end = 20;
-for (let i = start; i <= end; i++){
-    if (i % 2 == 0) {
-        console.log(i)
-    }
+// const start = 10;
+// const end = 20;
+// for (let i = start; i <= end; i++){
+//     if (i % 2 == 0) {
+//         console.log(i)
+//     }
+// } 
+
+const numbers = [10, 15, 20, 30];
+let totalSum = 0;
+for (let i = 0; i < numbers.length; i++) {
+  totalSum += numbers[i];
 }
+
+console.log(totalSum);
