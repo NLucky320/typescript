df59f8de8cb466234d641499eb8db66a4bdefb23
Nlucky320 <luckyes144@gmail.com>
2024-10-08 12:14:09 +0600

    new commit

--- ex.js
+++ ex.js
@@ -298,4 +298,16 @@ console.log(greet)
 function positiveSum(arr) {
 return arr.reduce((accumulator, currentValue)=>(currentValue>0?accumulator+currentValue:accumulator), 0)
 }
+
+function positiveSum(arr) {
+  var total = 0;
+  for (i = 0; i < arr.length; i++) {
+    // setup loop to go through array of given length
+    if (arr[i] > 0) {
+      // if arr[i] is greater than zero
+      total += arr[i]; // add arr[i] to total
+    }
+  }
+  return total; // return total
+}
 console.log(positiveSum([]))
